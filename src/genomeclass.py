6dc10958775676c7507b21256e2bef575f5f7fe2
mirakaya <59024914+mirakaya@users.noreply.github.com>
2025-09-18 20:04:00 +0100

    Update.

--- src/genomeclass.py
+++ src/genomeclass.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import itertools
 import pickle
 import os
@@ -18,8 +19,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from xgboost import XGBClassifier
 import matplotlib.pyplot as plt
 
-def import_files(filename):  # import the csv file
+def import_files(filename, sep):  # import the csv file
 
+	#Read the tsv file
 	chunks = pd.read_csv(filename, sep='\t', low_memory=False, chunksize=500000)
 	data = pd.concat(chunks, ignore_index=True)
 
@@ -27,9 +29,9 @@ def import_files(filename):  # import the csv file
 
 	data['Sequence_id'] = data['Sequence_id'].str.replace('>', '', regex=False)
 
-	if args.s != None:
+	if sep != None:
 
-		data['Sequence_id'] = data['Sequence_id'].str.split('|').str[args.s]
+		data['Sequence_id'] = data['Sequence_id'].str.split('|').str[sep]
 
  	# Remove rows with empty values on the target feature
 	data = data[data['Sequence_id'].notna() & (data['Sequence_id'].str.strip() != '')]
@@ -40,9 +42,14 @@ def import_files(filename):  # import the csv file
 def drop_columns(data):
 	le = LabelEncoder()
 	X = data.drop(columns=['Sequence_id'])
-	y = le.fit_transform(data['Sequence_id'])
 
-	return X, y
+	try:
+		y = le.fit_transform(data['Sequence_id'])
+	except:
+		y = None
+		le = None
+
+	return X, y, le
 
 
 def print_to_files(info):
@@ -66,10 +73,11 @@ def write_to_file(string_to_write):
 		file.write(string_to_write)
 
 
-def fit_and_predict(model, name, is_test):
+def fit_and_predict(model, name, is_test, X_train, y_train, X_test = None, y_test = None):
+
 	if is_test == True:
 
-		scores, mean_score, std_dev = calculate_cv_scores(model, name)
+		scores, mean_score, std_dev = calculate_cv_scores(model, name, X_train, y_train)
 
 
 		# Train the model
@@ -116,11 +124,13 @@ def fit_and_predict(model, name, is_test):
 	else:
 		print("Saving the " + name + "...")
 
-		model.fit(X, Y)
+		ml_model = model.fit(X_train, y_train)
 
 		# save model
 		filename = name + '.sav'
 		pickle.dump(model, open(filename, 'wb'))
+		
+		return ml_model
 
 
 def balance_data(df):
@@ -146,6 +156,17 @@ def balance_data(df):
 
 			# Drop those rows
 			df = df.drop(index=indices_to_remove)
+
+	return df
+
+def min_balancing(df):
+
+	target = 'Sequence_id'
+
+	for value in df[target].unique():
+
+		if (df[target] == value).sum() <= 2:
+			df = df[df[target] != value]
 
 	return df
 
@@ -179,7 +200,7 @@ def PCA_feature_analysis():
 	print(loadings['PC1'].abs().sort_values(ascending=False).head(10))
 
 
-def calculate_cv_scores(model, name):
+def calculate_cv_scores(model, name, X_train, y_train):
 	# Print model name
 	print("\n\nTesting the " + name + "...")
 
@@ -196,17 +217,28 @@ def calculate_cv_scores(model, name):
 
 	return scores, mean_score, std_dev
 
+def write_predictions(X_column, predictions, name_file):
+	results_df = pd.DataFrame(X_column)
+	results_df.insert(0, 'predicted_value', predictions)
+
+	# --- Save to TSV file ---
+	results_df.to_csv(name_file, sep='\t', index=False)
+
+	print("Predictions saved to '" + name_file + "'")
+
+
+
 
 if __name__ == '__main__':
 
 	warnings.filterwarnings("ignore")
 
 	parser = argparse.ArgumentParser(description="Index", usage="Training and testing\n\npython3 genomeclass.py -f <input multi-FASTA file> -i <input (multi-)FASTA file>\n"
-	                                                            "python3 genomeclass.py -t <input TSV file> -i <input (multi-)FASTA file>\n")
+																"python3 genomeclass.py -t <input TSV file> -i <input (multi-)FASTA file>\n")
 
 	parser.add_argument("-f", help="Input multi-FASTA file", type=str)
 	parser.add_argument("-t", help="Input TSV file", type=str)
-	parser.add_argument("-i", help="Input FASTA file containing the sequences to be classified", nargs="+", type=str, required=False)
+	parser.add_argument("-i", help="Input FASTA file containing the sequences to be classified", type=str, required=False)
 	parser.add_argument("-s", help="Part of the Sequence_id that will become the target feature", type=int)
 	parser.add_argument("-m", help="Machine learning model to be used. Default: RandomForestClassifier", type=str)
 	parser.add_argument("-o", help="Options for the execution of the C file. Please surround the options with \"\"", type=str)
@@ -230,19 +262,26 @@ if __name__ == '__main__':
 	else:
 		options = "-s -g -c -e -m -t 7" + permutations_added
 
+	print(os.path.exists(args.i))
+
 	if args.f is not None and os.path.exists(args.f) and args.i is not None and os.path.exists(args.i):
 
-		print("Using " + args.f + " as the input file.\n")
-		print("File to be classified: " + args.i[0] + "\n")
+		print("Using " + args.f + " as the training file and " + args.i + " as the file to be classified.\n")
+		#print("File to be classified: " + args.i[0] + "\n")
 		os.system("make clean")
 		os.system("make")
-		print("\n./genomeclass -i " + args.f + " " + options + "\n\n")
-		os.system("./genomeclass -i " + args.f + " " + options)
-		dataset_name = "output.tsv"
 
-		print("\n./genomeclass -i " + args.i + " " + options + " -o test.tsv\n\n")
-		os.system("./genomeclass -i " + args.i + " " + options + " -o test.tsv")
+		print("Analysing the training file.\n")
+		dataset_name = "output_test.tsv"
+		print("\n./genomeclass -i " + args.f + " " + options + " -o " + dataset_name + "\n\n")
+		os.system("./genomeclass -i " + args.f + " " + options + " -o " + dataset_name)
+
+
+		print("Analysing the file to be classified.\n")
 		test_irl = "test.tsv"
+		print("\n./genomeclass -i " + args.i + " " + options + " -o " + test_irl + "\n\n")
+		os.system("./genomeclass -i " + args.i + " " + options + " -o " + test_irl)
+
 
 	elif args.t is not None and os.path.exists(args.t):
 		print("Using " + args.t + " as the input file.\n")
@@ -252,49 +291,36 @@ if __name__ == '__main__':
 		print("Invalid input files. Exiting.")
 		exit(1)
 
-
-
-	pd.set_option('display.max_columns', 30)
-
+	# Create the file that will hold the statistics for the training set
 	file_tsv = "performance_genomeclass_testing.tsv"
 
 	if os.path.exists(file_tsv):
 		os.remove(file_tsv)
 
+	write_to_file("S" + str(args.s) + "_Name_model\tScores_CV\tMean_score_CV\tStd_deviation\tAccuracy\tF1-score\tAUROC\tAUPRC\n")
 
-	write_to_file("Name_model\tScores_CV\tMean_score_CV\tStd_deviation\tAccuracy\tF1-score\tAUROC\tAUPRC\n")
+	# Import training data
+	data_tds = import_files(dataset_name, args.s)
 
-	data = import_files(dataset_name)
-
-	#print(data.dtypes)
-	print(data.shape)
-	#print(data.head(10))
-
-	if args.a:
-		data = balance_data(data)
-	data = additional_removals(data)
-
-	value_counts = data['Sequence_id'].value_counts()
+	# Minimal balancing, removes classes with 2 or less instances - avoids execution errors
+	data_tds = min_balancing(data_tds)
 	
-	for seq_id, count in value_counts.items():
-		print(f"{seq_id}: {count}")
-
-	print(data['Sequence_id'].value_counts())
-
-	print(f"Number of unique values: {data['Sequence_id'].nunique()}")
-	data = data.loc[:, ~data.columns.str.startswith('Prob_sequence_')]
-
-
-	# Start training process
-	print(data.shape)
+	# Balances the data if wanted
+	if args.a:
+		data_tds = balance_data(data_tds)
+	data_tds = additional_removals(data_tds)
+	
+	# Removes the columns that begin with Prob_sequence as they hold information similar to those that begin with Avg_distance
+	data_tds = data_tds.loc[:, ~data_tds.columns.str.startswith('Prob_sequence_')]
+	
+	# -> Train the models on the training dataset
+	print(data_tds.shape)
 
 	# Separate features and target first
-	X, Y = drop_columns(data)
-
-	PCA_feature_analysis()
+	X_tds, Y_tds, le = drop_columns(data_tds)
 
 	# Train/test split (stratified)
-	X_train, X_test, y_train, y_test = train_test_split(X, Y, stratify=Y, test_size=0.2, random_state=42)
+	X_train_tds, X_test_tds, y_train_tds, y_test_tds = train_test_split(X_tds, Y_tds, stratify=Y_tds, test_size=0.2, random_state=42)
 
 	# Set models
 	xgboost = XGBClassifier(random_state=42)
@@ -303,40 +329,49 @@ if __name__ == '__main__':
 	mlp = MLPClassifier(random_state=42)
 
 	# Get performance in the cross validation and train the models on the entire training set
-
-	fit_and_predict(xgboost, "XGBClassifier", True)
-	fit_and_predict(random_forest, "RandomForestClassifier", True)
-	fit_and_predict(knn, "KNeighborsClassifier", True)
-	fit_and_predict(mlp, "MLPClassifier", True)
+	fit_and_predict(xgboost, "XGBClassifier", True, X_train_tds, y_train_tds, X_test_tds, y_test_tds)
+	fit_and_predict(random_forest, "RandomForestClassifier", True, X_train_tds, y_train_tds, X_test_tds, y_test_tds)
+	fit_and_predict(knn, "KNeighborsClassifier", True, X_train_tds, y_train_tds, X_test_tds, y_test_tds)
+	fit_and_predict(mlp, "MLPClassifier", True, X_train_tds, y_train_tds, X_test_tds, y_test_tds)
 
 
 	# Save the models
-
-	'''fit_and_predict(xgboost, "XGBClassifier", False)
-	fit_and_predict(random_forest, "RandomForestClassifier", False)
-	fit_and_predict(knn, "KNeighborsClassifier", False)
-	fit_and_predict(mlp, "MLPClassifier", False)'''
-
+	xgbmodel_trained = fit_and_predict(xgboost, "XGBClassifier", False, X_tds, Y_tds)
+	rfmodel_trained = fit_and_predict(random_forest, "RandomForestClassifier", False, X_tds, Y_tds)
+	knnmodel_trained = fit_and_predict(knn, "KNeighborsClassifier", False, X_tds, Y_tds)
+	mlpmodel_trained = fit_and_predict(mlp, "MLPClassifier", False, X_tds, Y_tds)
 
 
-	'''if args.i is not None:
-		X_train = X
-		y_train = Y
+	# If there is real data to be classified
+	if args.i != None:
 
-		test_data = import_files(args.i)
+		# Import data to be classified
+		data_clf = import_files(test_irl, args.s)
 
-		X_test, y_test = drop_columns(test_data)
+		# Removes the columns that begin with Prob_sequence as they hold information similar to those that begin with Avg_distance
+		data_clf = data_clf.loc[:, ~data_clf.columns.str.startswith('Prob_sequence_')]
 
-		#xgboost = XGBClassifier(random_state=42)
-		#fit_and_predict(xgboost, "XGBClassifier", True)
+		# -> Train the models on the training dataset
+		print(data_clf.shape)
 
-		random_forest = RandomForestClassifier(random_state=42)
-		fit_and_predict(random_forest, "RandomForestClassifier", True)
+		# Separate features and target first
+		X_clf, Y_clf, le_clf = drop_columns(data_clf)
 
-		knn = KNeighborsClassifier()
-		fit_and_predict(knn, "KNeighborsClassifier", True)
+		# Predict the classifications
+		predictions_xgb = xgbmodel_trained.predict(X_clf)
+		predictions_rf = rfmodel_trained.predict(X_clf)
+		predictions_knn = knnmodel_trained.predict(X_clf)
+		predictions_mlp = mlpmodel_trained.predict(X_clf)
 
-		mlp = MLPClassifier(random_state=42)
-		fit_and_predict(mlp, "MLPClassifier", True)
-'''
+		predicted_labels_xgb = le.inverse_transform(predictions_xgb)
+		predicted_labels_rf = le.inverse_transform(predictions_rf)
+		predicted_labels_knn = le.inverse_transform(predictions_knn)
+		predicted_labels_mlp = le.inverse_transform(predictions_mlp)
+
+		# Write predictions to a TSV file
+		write_predictions(data_clf, predicted_labels_xgb, "predictions_xgboost.tsv")
+		write_predictions(data_clf, predicted_labels_rf, "predictions_randforest.tsv")
+		write_predictions(data_clf, predicted_labels_knn, "predictions_knn.tsv")
+		write_predictions(data_clf, predicted_labels_mlp, "predictions_mlp.tsv")
+
 
